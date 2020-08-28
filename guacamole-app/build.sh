0606c68b2b1d9a3a06c0ee1c908357ef8bd551d5
Thomas Deutsch <thomas@tuxpeople.org>
2020-08-28 08:11:49 +0200

    changes to fit my needs

--- guacamole-app/build.sh
+++ guacamole-app/build.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 cd branding
 jar cf ../branding.jar .
 cd ..
-docker build -t d2iqshadowbq/guacamole-branded:latest -t d2iqshadowbq/guacamole-branded:v1.1.0 .
-docker push d2iqshadowbq/guacamole-branded:v1.1.0
+docker build -t tdeutsch/guacamole-branded:latest -t tdeutsch/guacamole-branded:v1.1.0 .
+docker push tdeutsch/guacamole-branded:v1.1.0
+docker push tdeutsch/guacamole-branded:latest
